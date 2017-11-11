28d5f18b69e31d7c021da7612a2dd68e858046e5
Akhil <akhil.sudh@gmail.com>
2017-11-11 23:57:13 +0530

    Added 5b

---
+++ 5b.pl
@@ -0,0 +1,10 @@
+#!/usr/bin/perl
+
+print "content-type:text/html\n\n";
+use CGI;
+$cgi = new CGI;
+$cmd = $cgi -> param("cmd");
+print "<html><bodybgcolor=lightgreen><center><b>";
+print "output of the executed UNIX command :<br><br>";
+system($cmd);
+print "</b></center></body></html>";
