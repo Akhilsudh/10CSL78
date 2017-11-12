a35bdf03fddadc2b53da5dddcebb38f6c52e0591
Akhil <akhil.sudh@gmail.com>
2017-11-12 19:57:02 +0530

    Fixed refreshing of screen for 7.pl

--- 7.pl
+++ 7.pl
@@ -2,7 +2,7 @@
 
 print "content-type:text/html\n\n";
 print "<html><title>Digital Clock</title><head>";
-print "<META HTTP-EQUIV=refresh content=0";
+print "<META HTTP-EQUIV=refresh content=0;";
 print "URL=http://localhost/cgi-bin/7.pl>";
 ($s,$m,$h) = localtime(time);
 print "<body bgcolor=lightgreen><center><b>";
